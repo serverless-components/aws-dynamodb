16c9f8e163ddb7488b3b4c84aad478ba29d86e4e
Barry Steyn <bsteyn@linkedin.com>
2019-10-23 10:37:24 -0700

    GlobalSecondary and LocalSecondary indexes

--- serverless.js
+++ serverless.js
@@ -18,6 +18,7 @@ const defaults = {
       KeyType: 'HASH'
     }
   ],
+  globalSecondaryIndexes: [],
   name: false,
   region: 'us-east-1'
 }
@@ -79,12 +80,15 @@ class AwsDynamoDb extends Component {
         if (!equals(prevTable.name, config.name)) {
           // If "delete: false", don't delete the table
           if (config.delete === false) {
-            throw new Error(`You're attempting to change your table name from ${this.state.name} to ${config.name} which will result in you deleting your table, but you've specified the "delete" input to "false" which prevents your original table from being deleted.`)
+            throw new Error(
+              `You're attempting to change your table name from ${this.state.name} to ${config.name} which will result in you deleting your table, but you've specified the "delete" input to "false" which prevents your original table from being deleted.`
+            )
           }
           await deleteTable({ dynamodb, name: prevTable.name })
           config.arn = await createTable({ dynamodb, ...config })
         } else {
-          await updateTable({ dynamodb, ...config })
+          const prevGlobalSecondaryIndexes = prevTable.globalSecondaryIndexes || []
+          await updateTable.call(this, { dynamodb, prevGlobalSecondaryIndexes, ...config })
         }
       }
     }
